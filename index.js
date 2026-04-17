470d497baef25e141e3ddcd776227ec85b5605b4
YASKAJR <moyasin292@gmail.com>
2026-04-17 10:03:49 +0300

    Completed combineUser function with spread and datejs

--- index.js
+++ index.js
@@ -1,5 +1,19 @@
+require('datejs');
 
+function combineUsers(...args) {
+  const combinedObject = {
+    users: []
 
+  };
+  args.forEach(userArray => {
+    combinedObject.users = [...combinedObject.users, ...userArray]; 
+
+  });
+  combinedObject.merge_date =Date.today().toString('M/d/yyyy');
+
+  return combinedObject;
+
+}
 
 module.exports = {
   ...(typeof combineUsers !== 'undefined' && { combineUsers })
